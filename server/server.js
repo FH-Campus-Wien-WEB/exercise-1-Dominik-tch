1ac0cdf0534ccba45d628cd6714bf053d3409682
dominik.tch <dominik.tasch04@gmail.com>
2026-03-23 12:02:09 +0100

    added example movie jsons

--- server/server.js
+++ server/server.js
@@ -7,8 +7,46 @@ app.use(express.static(path.join(__dirname, 'files')));
 
 // Configure a 'get' endpoint for data..
 app.get('/movies', function (req, res) {
-  // Part 1: Remove the next line and replace with your code
-  res.send('!dlrow olleH')
+  const movieJson = [
+                  {Title:"Iron Man",
+                    Released:"2008-05-02",
+                    Runtime: 126,
+                    Genres:["Action", "Adventure", "Sci-Fi"],
+                    Directors:["Jon Favreau"],
+                    Writers:["Mark Fergus", "Hawk Ostby", "Art Marcum"],
+                    Actors:["Robert Downey Jr.", "Gwyneth Paltrow", "Terrence Howard"],
+                    Plot:"After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+                    Poster:"https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg",
+                    Metascore:79,
+                    imdbRating:7.9
+                  },
+                  {
+                    Title: "Inception",
+                    Released: "2010-07-16",
+                    Runtime: 148,
+                    Genres: ["Action", "Adventure", "Sci-Fi"],
+                    Directors: ["Christopher Nolan"],
+                    Writers: ["Christopher Nolan"],
+                    Actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page"],
+                    Plot: "A thief who steals corporate secrets through dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+                    Poster: "https://m.media-amazon.com/images/M/MV5BMmEzNTYzNmUtYzY1My00ZjQ3LWFmNTEtODM1ZmRlYzQ0ZTRkXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+                    Metascore: 74,
+                    imdbRating: 8.8
+                  },
+                  {
+                    Title: "The Matrix",
+                    Released: "1999-03-31",
+                    Runtime: 136,
+                    Genres: ["Action", "Sci-Fi"],
+                    Directors: ["Lana Wachowski", "Lilly Wachowski"],
+                    Writers: ["Lana Wachowski", "Lilly Wachowski"],
+                    Actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
+                    Plot: "A hacker discovers the reality he lives in is a simulation and joins a rebellion against the machines controlling humanity.",
+                    Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ3Zi00ODZmLTk3YTItNDNkZTk3ZDE4ZTRhXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg",
+                    Metascore: 73,
+                    imdbRating: 8.7
+                  }]
+    res.json(movieJson)
 })
 
 app.listen(3000)
